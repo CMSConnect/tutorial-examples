71b0a6e12ee07758f96a6ff97f2bddf106337e76
khurtado <khurtado@nd.edu>
2017-11-30 02:17:24 -0600

    New condor examples

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# tutorial-examples
-Additional condor examples
+# Additional condor examples
+
+## example01.submit
+Job with a single input and output. Transfer output file via condor.
+
+## example02.submit
+Jobs with multiple input files to be transferred and output filenames remap
+
+## example03.submit
+Jobs with multiple input files from a text file and output filenames remap
---
+++ example03.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# short.sh: a short discovery job 
+
+dummy_analysis(){
+  echo "Input filename to open: $1" > "$2"
+  echo "Dummy result: $RANDOM" >> "$2"
+}
+
+printf "Start time: "; /bin/date 
+printf "Job is running on node: "; /bin/hostname 
+printf "Job running as user: "; /usr/bin/id 
+printf "Job is running in directory: "; /bin/pwd 
+
+echo
+echo "Working hard..."
+dummy_analysis "$1" output.txt
+
+echo "Science complete!"
